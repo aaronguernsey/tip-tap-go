8ceb4302c5cc0d4c8f90a7602fe879945177a32a
Aaron Guernsey <guernsey.a@gmail.com>
2022-10-16 20:39:37 -0500

    chore(meta): update description content

--- constants/content.ts
+++ constants/content.ts
@@ -3,7 +3,7 @@ export const GAME_TITLE = "Tip Tap Go";
 // SEO Meta CONTENT
 export const META_TITLE = `Let's Play ${GAME_TITLE}`;
 export const META_DESCRIPTION =
-  "Clear as many blocks before the time runs out.";
+  "Clear as many blocks before the time runs out. Share your streaks with friends and family!";
 
 export const BUTTON_PLAY_AGAIN = "Play again";
 export const START_TITLE = "Let's Play";
