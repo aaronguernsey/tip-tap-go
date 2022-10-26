829335293f9a7e4736f09ae0f9ad03fc6860d98c
Aaron Guernsey <guernsey.a@gmail.com>
2022-10-25 21:21:54 -0500

    chore(analytics): add vercel beta analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AlertProvider } from "../context/AlertContext";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AlertProvider>
       <Component {...pageProps} />
+      <Analytics />
     </AlertProvider>
   );
 }
